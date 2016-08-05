f01b5defa6bcba99bcca12a899e869638d236e10
Esteban Mino <efmino@uc.cl>
2016-08-04 23:12:51 -0300

    responsive navbar

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,19 +1,16 @@
-
-
-
 $(document). on("click", function(){
   if($("#sidebar-wrapper").hasClass('active')){
       $("#sidebar-wrapper").removeClass("active");
   }
             });
 
-$(document). on("click", '#menu-close', function(e){
+$(document). on("click", '#sidebar-close', function(e){
         e.stopPropagation();
           $("#sidebar-wrapper").removeClass("active");
             });
 
 
-$(document). on("click", '#menu-toggle', function(e){
+$(document). on("click", '#sidebar-toggle', function(e){
          e.stopPropagation();
           $("#sidebar-wrapper").toggleClass("active");
             });
